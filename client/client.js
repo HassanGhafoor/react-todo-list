c580479a6d6d4f7dc7022ad583a08f2af623de48
HassanGhafoor <60030532+HassanGhafoor@users.noreply.github.com>
2024-10-10 02:33:50 +0500

    Update client.js

--- client/client.js
+++ client/client.js
@@ -1,1 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
+// Renders the App component into the #app div in index.html
+ReactDOM.render(<App />, document.getElementById('app'));
